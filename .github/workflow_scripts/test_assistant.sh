218e57d93e3bc078712616d43cdf55c94e79cd24
Su Zhou <zhousu@amazon.com>
2024-12-18 11:43:19 -0800

    Update test_assistant.sh
    Display print()

--- .github/workflow_scripts/test_assistant.sh
+++ .github/workflow_scripts/test_assistant.sh
@@ -8,4 +8,4 @@ source $(dirname "$0")/env_setup.sh
 
 setup_test_env
 
-python -m pytest -n 2 -v --capture=tee-sys --log-cli-level=INFO --junitxml=results.xml tests/unittests/$MODULE/
+python -m pytest -n 2 -v --capture=no --log-cli-level=INFO --junitxml=results.xml tests/unittests/$MODULE/
