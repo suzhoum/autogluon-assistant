f765a628d15f4bc02b44f67a92be62139a889d85
Su Zhou <zhousu@amazon.com>
2024-12-18 11:33:29 -0800

    Update test_assistant.sh

--- .github/workflow_scripts/test_assistant.sh
+++ .github/workflow_scripts/test_assistant.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-MODULE=$1
+MODULE=integration
 
 set -ex
 
 source $(dirname "$0")/env_setup.sh
 
-install_all
 setup_test_env
 
 python -m pytest -n 2 -v --capture=tee-sys --log-cli-level=INFO --junitxml=results.xml tests/unittests/$MODULE/
